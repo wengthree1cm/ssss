bcfc6cd7d979adda8a77fcebe8e9f6e5384bae26
wengthree1cm <chl135@ucsd.edu>
2022-04-22 00:33:02 -0700

    file3 debug

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,18 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if(markdown.indexOf("(")==-1){
+                return toReturn;
+            }
+            else{
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            }
+            
         }
 
         return toReturn;
